786b40985920c130f3b124e3cd6d40b887e01590
JoaquinFritz16 <d48125437@alumnos.itr.edu.ar>
2025-04-10 12:34:53 -0300

    Trabajo de buffer

--- Simulacion_Buffer3.py
+++ Simulacion_Buffer3.py
@@ -5,7 +5,7 @@ class Nodo:
     def __init__(self, nombre):
         self.nombre = nombre
         self.conected = []
-        self.buffer = []  # Buffer para almacenar mensajes pendientes
+        self.buffer = []
 
     def agregar_conexion(self, nodo):
         if nodo not in self.conected:
@@ -20,17 +20,17 @@ class Nodo:
                 nodo.recibir_message(message, self)
 
     def recibir_message(self, message, nodo):
-        # Simular pérdida de paquetes con random
-        if random.random() > 0.2:  # 80% de probabilidad de recibir el mensaje
+        
+        if random.random() > 0.2:
             print(f"{self.nombre} ha recibido un mensaje de {nodo.nombre}: '{message}'")
-            self.buffer.append((message, nodo))  # Almacenar mensaje en el buffer
+            self.buffer.append((message, nodo))
         else:
             print(f"{self.nombre} perdió un mensaje de {nodo.nombre}: '{message}'")
 
     def procesar_buffer(self):
         print(f"{self.nombre} procesando mensajes en el buffer...")
         while self.buffer:
-            message, nodo = self.buffer.pop(0)  # Procesar el primer mensaje en el buffer
+            message, nodo = self.buffer.pop(0)
             print(f"{self.nombre} procesó el mensaje de {nodo.nombre}: '{message}'")
 
     def remove_conection(self, nodo):
@@ -41,19 +41,18 @@ class Nodo:
         else:
             print(f"{self.nombre} no está conectado a {nodo.nombre}")
 
-# Simulación
 server = Nodo("Servidor")
 cliente1 = Nodo("Cliente 1")
 cliente2 = Nodo("Cliente 2")
 cliente3 = Nodo("Cliente 3")
 
-# Conexiones iniciales
 server.agregar_conexion(cliente1)
 server.agregar_conexion(cliente2)
 cliente1.agregar_conexion(cliente3)
 
 
 print("\n--- Enviando mensajes ---")
+time.sleep(2)
 server.enviar_message("Hola, ¿cómo estás?")
 cliente1.enviar_message("Hola, estoy bien. ¿Y tú?")
 cliente2.enviar_message("No me importa cliente 1, trabaja más")
@@ -61,7 +60,12 @@ cliente3.enviar_message("Hola, cliente 2. ¿Qué tal?")
 
 
 print("\n--- Procesando buffers ---")
+time.sleep(2)
 server.procesar_buffer()
 cliente1.procesar_buffer()
 cliente2.procesar_buffer()
 cliente3.procesar_buffer()
+time.sleep(2)
+cliente1.remove_conection(cliente3)
+print("Simulacion de conexion de cliente 1 y cliente 3")
+server.enviar_message("¡HOLA A TODOS DE NUEVO!")
